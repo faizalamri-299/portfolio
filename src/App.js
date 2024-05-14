6f88e4f4e945fcf8e9248c2b42cd5d37f38a2f3f
Faizal Amri <faizalmr33@gmail.com>
2024-05-14 15:06:33 +0800

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import About from './pages/About';
 import Skill from './pages/Skill';
 import Project from './pages/Project';
+import Contact from './pages/Contact';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const Portfolio = () => {
@@ -25,9 +26,15 @@ const Portfolio = () => {
         {/* Project Section */}
         <Project />
 
+
+        <Contact />
+
         {/* Floating Circles */}
-        <ParallaxLayer offset={0.3} speed={-0.2}>
-          <div className="absolute top-1/4 left-1/4 w-40 h-40 bg-white rounded-full opacity-50"></div>
+        <ParallaxLayer offset={0.3} speed={0.2}>
+          <div className="absolute inset-y-0 right-1/2 w-[40%] h-[45%] rounded-xl bg-white opacity-50"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={-0.1} speed={-0.2}>
+          <div className="absolute inset-y-0 top-18 right-44 w-[30%] h-[81%] rounded-xl bg-white opacity-50"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0.6} speed={-0.2}>
           <div className="absolute top-1/2 right-1/4 w-32 h-32 bg-white rounded-full opacity-50"></div>
@@ -40,6 +47,9 @@ const Portfolio = () => {
         </ParallaxLayer>
         <ParallaxLayer offset={1.5} speed={-0.2}>
           <div className="absolute bottom-1/2 right-1/2 w-44 h-44 bg-white rounded-full opacity-50"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={4} speed={-0.2}>
+          <div className="absolute bottom-1/2 right-1/2 w-44 h-44 bg-white rounded-xl opacity-50"></div>
         </ParallaxLayer>
 
         {/* Scroll to Top Button */}
